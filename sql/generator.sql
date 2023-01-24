0bdd7fdd1ccb20c9faff170dabaa8dc626ce47b1
Niklas Brandtner <73284390+nbrandtner-tgm@users.noreply.github.com>
2023-01-24 08:41:40 +0100

    Update generator for the new database layout

--- sql/generator.sql
+++ sql/generator.sql
@@ -1,12 +1,12 @@
-DROP TABLE IF EXISTS testdata;
-CREATE TABLE testdata(
+DROP TABLE IF EXISTS estats;
+CREATE TABLE estats(
    region               VARCHAR(20) NOT NULL
-  ,stromverbrauchanteil NUMERIC(20,9) NOT NULL
-  ,stromverbrauch       NUMERIC(20,4) NOT NULL
-  ,strompreis           BIGINT NOT NULL
-  ,CO2Emissionen        NUMERIC(20,9) NOT NULL
-  ,Stromimport          NUMERIC(20,9) NOT NULL
-  ,Stromexport          NUMERIC(20,9) NOT NULL
-  ,date                 INTEGER  NOT NULL
+  ,verbrauchAnteil      decimal(20,4) NOT NULL
+  ,verbrauch            decimal(20,4) NOT NULL
+  ,preis                BIGINT NOT NULL
+  ,emission             decimal(20,4) NOT NULL
+  ,import               decimal(20,4) NOT NULL
+  ,export               decimal(20,4) NOT NULL
+  ,date                 datetime  NOT NULL
   ,PRIMARY KEY(region,date)
 );
